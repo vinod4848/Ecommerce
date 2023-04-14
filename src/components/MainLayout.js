2aab9dd9a005de85a9e1a50dffab1ff669845c77
vinod4848 <vinodku4848@gmail.com>
2023-04-14 14:18:24 +0530

    add sum module and charts

---
+++ src/components/MainLayout.js
@@ -0,0 +1,212 @@
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
+import {
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { Outlet } from "react-router-dom";
+import { IoMdNotifications } from "react-icons/io";
+import { SiBrandfolder } from "react-icons/si";
+import { ImBlog } from "react-icons/im";
+import { BiCategoryAlt } from "react-icons/bi";
+import { GrUnorderedList } from "react-icons/gr";
+import { FaBlogger } from "react-icons/fa";
+import { Layout, Menu, theme } from "antd";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const { Header, Sider, Content } = Layout;
+const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(true);
+
+  const navigate = useNavigate();
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const tryShow = () => {
+    setCollapsed(!collapsed);
+
+    if (collapsed) {
+      setShow(false);
+      setShow2(true);
+    } else {
+      setShow(true);
+      setShow2(false);
+    }
+  };
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
+          <h2 className="text-white fs-5 text-center py-3 mb-0">
+            {show && <span className="sm-logo">Get</span>}
+            {show2 && <span className="gl-logo">Shopping</span>}
+          </h2>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key == "singnout") {
+            } else {
+              navigate(key);
+            }
+          }}
+          items={[
+            {
+              key: "",
+              icon: <AiOutlineDashboard className="fs-4" />,
+              label: "Dashbord",
+            },
+            {
+              key: "customers",
+              icon: <AiOutlineUser className="fs-4" />,
+              label: "Customers",
+            },
+            {
+              key: "catalog",
+              icon: <AiOutlineShoppingCart className="fs-4" />,
+              label: "Catalog",
+              children: [
+                {
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Add Product",
+                },
+                {
+                  key: "product-list",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
+                },
+                {
+                  key: "category",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand",
+                },
+                {
+                  key: "list-category",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand-List",
+                },
+                {
+                  key: "category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category",
+                },
+                {
+                  key: "list-category",
+                  icon: <BiCategoryAlt className="fs-4" />,
+                  label: "Category-list",
+                },
+                {
+                  key: "color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color",
+                },
+                {
+                  key: "list-color",
+                  icon: <AiOutlineBgColors className="fs-4" />,
+                  label: "Color-list",
+                },
+              ],
+            },
+            {
+              key: "orders",
+              icon: <GrUnorderedList className="fs-4" />,
+              label: "Orders",
+            },
+            {
+              key: "blogs",
+              icon: <FaBlogger className="fs-4" />,
+              label: "Blogs",
+              children: [
+                {
+                  key: "blogs",
+                  icon: <FaBlogger className="fs-4" />,
+                  label: "Add Blog",
+                },
+                {
+                  key: "blog-list",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Blog List",
+                },
+                {
+                  key: "blog-category",
+                  icon: <FaBlogger className="fs-4" />,
+                  label: "Add Blog Category",
+                },
+                {
+                  key: "blog-category-list",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Blog Category List",
+                },
+              ],
+            },
+            {
+              key: "enquiries",
+              icon: <GrUnorderedList className="fs-4" />,
+              label: "Enquiries",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="d-flex justify-content-between ps-3 pe-5"
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => {
+                tryShow();
+              },
+            }
+          )}
+          <div className="d-flex gap-4 align-items-center">
+            <div className="position-relative">
+              <IoMdNotifications className="fs-4" />
+              <span className="badge bg-warning rounded-circle p-1 position-absolute">
+                2
+              </span>
+            </div>
+            <div className="d-flex  gap-3 align-items-center">
+              <div className="img">
+                <img
+                  width={32}
+                  height={32}
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLzi8P9Z8RNkDxO7TSfCTcMN3PkJKyG9rMJw&usqp=CAU"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="mb-0">Abhi</h5>
+                <p className="mb-0">abhi@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default MainLayout;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,172 @@
+import React from "react";
+import { BsArrowDownRight } from "react-icons/bs";
+import { Table } from "antd";
+import { Column } from "@ant-design/plots";
+const Dashboard = () => {
+  const columns = [
+    {
+      title: "SNo",
+      dataIndex: "key",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Product",
+      dataIndex: "product",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
+  const data1 = [];
+  for (let i = 0; i < 46; i++) {
+    data1.push({
+      key: i,
+      name: `Edward King ${i}`,
+      product: 32,
+      status: `London, Park Lane no. ${i}`,
+    });
+  }
+  const data = [
+    {
+      type: "jan",
+      sales: 38,
+    },
+    {
+      type: "Fab",
+      sales: 52,
+    },
+    {
+      type: "Mar",
+      sales: 61,
+    },
+    {
+      type: "Apr",
+      sales: 145,
+    },
+    {
+      type: "May",
+      sales: 48,
+    },
+    {
+      type: "Jun",
+      sales: 38,
+    },
+    {
+      type: "July",
+      sales: 38,
+    },
+    {
+      type: "Aug",
+      sales: 38,
+    },
+    {
+      type: "Sept",
+      sales: 38,
+    },
+    {
+      type: "Oct",
+      sales: 38,
+    },
+    {
+      type: "Nov",
+      sales: 38,
+    },
+    {
+      type: "Dec",
+      sales: 38,
+    },
+  ];
+  const config = {
+    data,
+    xField: "type",
+    yField: "sales",
+    color: ({ type }) => {
+      return "#ffd333";
+    },
+    label: {
+      position: "middle",
+      style: {
+        fill: "#FFFFFF",
+        opacity: 1,
+      },
+    },
+    xAxis: {
+      label: {
+        autoHide: true,
+        autoRotate: false,
+      },
+    },
+    meta: {
+      type: {
+        alias: "months",
+      },
+      sales: {
+        alias: "income",
+      },
+    },
+  };
+  return (
+    <div>
+      <h3 className="mb-4">Dashboard</h3>
+      <div className="d-flex justify-content-between align-item-cemter gap-3">
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="red">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0"> Compare To April 2023</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6>
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0"> Compare To April 2023</p>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3">
+          <div>
+            <p className="">Total</p>
+            <h4 className="mb-0">$1100</h4>
+          </div>
+          <div className="d-flex flex-column align-items-end">
+            <h6 className="green">
+              <BsArrowDownRight />
+              32%
+            </h6>
+            <p className="mb-0"> Compare To April 2023</p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mb-4">Income Statics</h3>
+        <div>
+          <Column {...config} />;
+        </div>
+      </div>
+      <div className="mt-4">
+        <h3 className="mt-4">Recent Orders</h3>
+        <div>
+          <Table columns={columns} dataSource={data1} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
