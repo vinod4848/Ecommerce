66e805fd1d7d5a33c1b0024fa70d1e6420966905
vinod4848 <vinodku4848@gmail.com>
2023-04-29 16:54:24 +0530

    Add coupon and sun add apis

--- src/pages/AddBlog.js
+++ src/pages/AddBlog.js
@@ -1,72 +1,165 @@
-import React, { useState } from "react";
+import { React, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { CustomInput } from "../components/CustomInput";
-import ReactQuill from "react-quill";
+import Dropzone from "react-dropzone";
 import "react-quill/dist/quill.snow.css";
-import { InboxOutlined } from "@ant-design/icons";
-import { message, Upload } from "antd";
-const { Dragger } = Upload;
-const props = {
-  name: "file",
-  multiple: true,
-  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
-};
+import { dellImages, uploadImages } from "../features/upload/uploadSlice";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getblogCat } from "../features/blogCat/blogCatSlice";
+import { createBlog, resetState } from "../features/blog/blogSlice";
+
+let userSchema = Yup.object().shape({
+  title: Yup.string().required("Title is Required"),
+  description: Yup.string().required("Description is Required"),
+  category: Yup.string().required("Category is Required"),
+});
+
 const AddBlog = () => {
-  const [desc, setDesc] = useState();
-  const handleDesc = (e) => {
-    setDesc(e);
-  };
+  const navigate = useNavigate();
+  const [images, setimages] = useState();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getblogCat());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const blogCatState = useSelector((state) => state.blogCat.blogCats);
+  console.log(blogCatState);
+  const imgState = useSelector((state) => state.upload.images);
+  const newProduct = useSelector((state) => state.product);
+
+  const { isLoding, isError, isSuccess, createdProdduct } = newProduct;
+
+  useEffect(() => {
+    if (isSuccess && createdProdduct) {
+      toast.success("Product Added Successfully!");
+    }
+    if (isError) {
+      toast.error("Somthing want wrong!");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoding, isError, isSuccess]);
+
+  const img = [];
+  imgState.forEach((i) => {
+    img.push({
+      public_id: i.public_id,
+      url: i.url,
+    });
+  });
+  useEffect(() => {
+    formik.values.images = img;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [images, img]);
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+      category: "",
+      images: "",
+    },
+    validationSchema: userSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+      dispatch(createBlog(values));
+      formik.resetForm();
+      setimages(null);
+      setTimeout(() => {
+        dispatch(resetState())
+        navigate("/admin/blog-list");
+      }, 3000);
+    },
+  });
   return (
     <div>
       <h3 className="mb-4 title">Add Blog</h3>
-      <div className="">
-        <form action="">
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibited from
-              uploading company data or other banned files.
-            </p>
-          </Dragger>
-          <div className="mt-4">
-            <CustomInput type="text" label="Enter Your Blog Title" />
-          </div>
-          <select name="" className="form-control py-3 mb-3" id="">
-            <option value="">Select Blog Category</option>
-          </select>
-          <ReactQuill
+      <form onSubmit={formik.handleSubmit} className="d-flex gap-3 flex-column">
+        <CustomInput
+          type="text"
+          label="Enter Product Title"
+          name="title"
+          onChange={formik.handleChange("title")}
+          onBlur={formik.handleBlur("title")}
+          val={formik.values.title}
+        />
+        <div className="error">
+          {formik.touched.title && formik.errors.title}
+        </div>
+        <div>
+          <CustomInput
             theme="snow"
-            value={desc}
-            onChange={(evt) => {
-              handleDesc(evt);
-            }}
+            label="Enter Product Description"
+            name="description"
+            onChange={formik.handleChange("description")}
+            value={formik.values.description}
           />
-          <button
-            className="btn btn-success border-0 rounde-3 my-5"
-            type="submit"
+        </div>
+        <div className="error">
+          {formik.touched.description && formik.errors.description}
+        </div>
+        <select
+          name="category"
+          onChange={formik.handleChange("category")}
+          onBlur={formik.handleBlur("category")}
+          val={formik.values.category}
+          className="form-control py-3 mb-3"
+          id=""
+        >
+          <option value="">Select Category</option>
+          {blogCatState.map((i, j) => {
+            return (
+              <option key={j} value={i.title}>
+                {i.title}
+              </option>
+            );
+          })}
+        </select>
+        <div className="error">
+          {formik.touched.category && formik.errors.category}
+        </div>
+        <div className="bg-white border-1 p-5 text-center">
+          <Dropzone
+            onDrop={(acceptedFiles) => dispatch(uploadImages(acceptedFiles))}
           >
-            Add Blog
-          </button>
-        </form>
-      </div>
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        </div>
+        <div className="showimage d-flex flex-wrap gap-3">
+          {imgState?.map((i, j) => {
+            return (
+              <div className="position-relative " key={j}>
+                <button
+                  type="button"
+                  onClick={() => dispatch(dellImages(i.public_id))}
+                  className="btn-close position-absolute"
+                  style={{
+                    top: "10px",
+                    right: "10px",
+                    backgroundColor: "white",
+                  }}
+                ></button>
+                <img src={i.url} alt="" width={200} height={200}></img>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className="btn btn-success border-0 rounde-3 my-5"
+          type="submit"
+        >
+          Add Blog
+        </button>
+      </form>
     </div>
   );
 };
--- src/pages/Addblogcat.js
+++ src/pages/Addblogcat.js
@@ -1,13 +1,68 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
 import { CustomInput } from "../components/CustomInput";
+import { createBlogCat } from "../features/blogCat/blogCatSlice";
+import { resetState } from "../features/color/colorSlice";
+
+let userSchema = Yup.object().shape({
+  title: Yup.string().required("Brand is Required"),
+});
 
 const AddblogCategory = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const newBlogCat = useSelector((state) => state.blogCat);
+  const { isSuccess, isError, isLoding, createblogCat } = newBlogCat;
+  console.log(newBlogCat);
+  useEffect(() => {
+    if (isSuccess && createblogCat) {
+      toast.success("Blog Category Added Successfully!");
+    }
+    if (isError) {
+      toast.error("Somthing want wrong!");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSuccess, isError, isLoding]);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: userSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values));
+      dispatch(createBlogCat(values));
+      formik.resetForm();
+      setTimeout(() => {
+        dispatch(resetState())
+        navigate("/admin/blog-list");
+      }, 3000);
+    },
+  });
   return (
     <div>
       <h3 className="mb-4 title">Add Blog Category</h3>
       <div>
-        <form action="">
-          <CustomInput type="text" label="Enter Your Blog Category" />
+        <form
+          onSubmit={formik.handleSubmit}
+          className="d-flex gap-3 flex-column"
+        >
+          <CustomInput
+            type="text"
+            label="Enter Blog Category Title"
+            name="title"
+            onChange={formik.handleChange("title")}
+            onBlur={formik.handleBlur("title")}
+            val={formik.values.title}
+          />
+          <div className="error">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             className="btn btn-success border-0 rounde-3 my-5"
             type="submit"
